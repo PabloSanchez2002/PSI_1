9baa81443e7f06e49f45b0690331d35c8502193c
PabloS <pablosanchez2002@gmail.com>
2023-02-03 10:59:29 +0100

    Version inicial

--- django_projects/locallibrary/locallibrary/urls.py
+++ django_projects/locallibrary/locallibrary/urls.py
@@ -37,4 +37,4 @@ urlpatterns += [
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  

ec34130f0bd0c15ea86002c13b9380244fe05975
Pablo <pablosanchez2002@gmail.com>
2023-02-05 00:45:43 +0100

    Primera week hecha ya con psql

--- django_projects/locallibrary/locallibrary/settings.py
+++ django_projects/locallibrary/locallibrary/settings.py
@@ -81,6 +81,17 @@ DATABASES = {
     }
 }
 
+#The following environment variable, called DATABASE_URL, has to be defined
+#at the o.s. level: export DATABASE_URL =
+#postgres://alumnodb:alumnodb@localhost:5432/psi
+
+
+import dj_database_url
+db_from_env = dj_database_url.config(default='postgres://alumnodb:alumnodb@localhost:5432/psi', conn_max_age=500)
+
+DATABASES['default'].update(db_from_env)
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators

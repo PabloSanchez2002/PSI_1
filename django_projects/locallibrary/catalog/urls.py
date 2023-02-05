487ca21ae7c72b44f3ebc3f06ed514c7e5a0f698
Pablo <pablosanchez2002@gmail.com>
2023-02-05 19:09:56 +0100

    Segunda Week casi hecha

--- django_projects/locallibrary/catalog/urls.py
+++ django_projects/locallibrary/catalog/urls.py
@@ -1,6 +1,35 @@
-from django.urls import path
+from django.urls import *
 from . import views
 
-urlpatterns = [
 
+
+urlpatterns = [
+    path('catalog/', include('catalog.urls')),
 ]
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+]
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+]
+
+urlpatterns += [
+   path('', views.index, name='index'),
+   path('authors/', views.AuthorListView.as_view(), name='authors'),
+]
+
+urlpatterns += [
+  path('', views.index, name='index'),
+  path('authors/', views.AuthorListView.as_view(), name='authors'),
+  path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+]
+

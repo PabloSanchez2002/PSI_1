ed625a52df782a2e11718affa433ee396150c974
Pablo <pablosanchez2002@gmail.com>
2023-02-05 14:52:59 +0100

    Segunda Week empezamos

--- django_projects/locallibrary/catalog/admin.py
+++ django_projects/locallibrary/catalog/admin.py
@@ -8,20 +8,18 @@ admin.site.register(Genre)
 # Define the admin class
 
 
+class BookInline(admin.TabularInline):
+    model = Book
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name',
                     'date_of_birth', 'date_of_death')
 
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+    inlines = [BookInline]
+
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
-
-
-
-# Register the Admin classes for Book using the decorator
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'display_genre')
-
 
 
 # Register the Admin classes for BookInstance using the decorator
@@ -29,3 +27,20 @@ class BookAdmin(admin.ModelAdmin):
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
     list_display = ('book', 'status', 'due_back', 'id')
+    fieldsets = (
+        (None, {'fields': ('book', 'imprint', 'id')}),
+        ('Availability', {'fields': ('status', 'due_back')}),
+    )
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+    inlines = [BooksInstanceInline]
+
+

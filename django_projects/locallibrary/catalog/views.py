5494e80b28093fb216b73904a739a52f016e1144
PabloS <pablosanchez2002@gmail.com>
2023-02-20 12:22:09 +0100

    TES PERFECTOS

--- django_projects/locallibrary/catalog/views.py
+++ django_projects/locallibrary/catalog/views.py
@@ -109,7 +109,7 @@ def renew_book_librarian(request, pk):
         # Check if the form is valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
-            book_instance.due_back = form.cleaned_data['due_back']
+            book_instance.due_back = form.cleaned_data['renewal_date']
             book_instance.save()
 
             # redirect to a new URL:
@@ -117,8 +117,8 @@ def renew_book_librarian(request, pk):
 
     # If this is a GET (or any other method) create the default form.
     else:
-        proposed_due_back = datetime.date.today() + datetime.timedelta(weeks=3)
-        form = RenewBookForm(initial={'due_back': proposed_due_back})
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
 
     context = {
         'form': form,

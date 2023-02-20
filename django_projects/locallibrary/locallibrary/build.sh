cee0298fe0c285d19680955f941c889a22d1b0e5
Pablo <pablosanchez2002@gmail.com>
2023-02-20 22:44:15 +0100

    VERSION FINAL 3

---
+++ django_projects/locallibrary/locallibrary/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o errexit  # exit on error
+
+pip install -r requirements.txt
+
+
+python3 manage.py collectstatic --no-input
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+export DJANGO_SUPERUSER_EMAIL=alumnodb@alumnodb.com
+export DJANGO_SUPERUSER_USERNAME=alumnodb
+export DJANGO_SUPERUSER_PASSWORD=alumnodb
+
+python3 manage.py createsuperuser --noinput
